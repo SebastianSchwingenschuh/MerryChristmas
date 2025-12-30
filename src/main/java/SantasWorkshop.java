786584955d4ebafbc155bde6b2edbba0961924eb
SebastianSchwingenschuh <s.schwingenschuh@students.htl-leonding.ac.at>
2025-12-30 14:42:38 +0100

    schwere geburt aber factory (Santas workshop) alle tests grün

--- src/main/java/SantasWorkshop.java
+++ src/main/java/SantasWorkshop.java
@@ -1,7 +1,51 @@
+import java.util.Objects;
+
 public class SantasWorkshop implements PresentFactory {
 
     @Override
     public Present createFromString(String s) {
-        return null;
+        //Present present = presentFactory.createFromString("[====]* Skateboard (8-30) for Bart");
+        // get size
+        if(!s.contains("=") || !s.contains("(") || !s.contains(")")) {
+            return null;
+        }
+        int size = s.lastIndexOf("=");
+        String[] words = s.split(" ");
+        String content = getStrBetween(s, "*", "(").trim(); // falls mehr wörter
+
+        Present newPresent;
+        if(Objects.equals(words[words.length - 2], "for")){
+            newPresent = new Present(content, size, words[words.length - 1]);
+        }
+        else {
+            newPresent = new Present(content, size);
+        }
+
+        String ageStr = getStrBetween(s, "(", ")");
+        if(ageStr.contains("+")){
+            ageStr = ageStr.replace("+", "");   //+ löschen
+            int minAge = Integer.parseInt(ageStr);
+            newPresent.setMinAge(minAge);
+            return  newPresent;
+        }
+        else {
+            String[] ageStrSplit = ageStr.split("-");
+            int minAge = Integer.parseInt(ageStrSplit[0]);
+            newPresent.setMinAge(minAge);
+
+            int maxAge = Integer.parseInt(ageStrSplit[1].replace("+", ""));
+            newPresent.setMaxAge(maxAge);
+        }
+        return newPresent;
     }
+
+    private String getStrBetween(String s ,String a, String b) {
+        int start = s.indexOf(a);
+        int end = s.indexOf(b);
+        String res = s.substring(start + 1, end);
+
+        return res;
+    }
+
+
 }
